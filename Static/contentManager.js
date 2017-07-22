2908163442117b40cf26e10b906344216eb878d5
fourteenfour hundred <xk9wdefrvnr343nfnd@gmail.com>
2017-07-22 18:52:11 -0500

    Logout
    Logout works

--- Static/contentManager.js
+++ Static/contentManager.js
@@ -41,8 +41,13 @@ function loadSubtopic(id,topic,active){
     });
 }
 
+function deleteCookie(name) {
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
 function logout(){
-   deleteCookie("session"); 
+//deleteCookie("session"); 
+    deleteCookie("session");
 }
 
 function showMore(topic,id,page){
